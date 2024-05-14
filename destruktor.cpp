1825c08892b59741b2aa8b6d6492ea0e15436c59
ahmadjunioor <161430813+ahmadjunioor@users.noreply.github.com>
2024-05-14 16:00:39 +0700

    membuat int main

--- destruktor.cpp
+++ destruktor.cpp
@@ -24,3 +24,25 @@ angka ::~angka(){
     delete[]arr;
     cout<<"Alamat Array Sudah Dilepaskan"<<endl;
 }
+
+void angka::cetakData(){
+    for(int i=1;i<=panjang;i++){
+        cout<<i<<" = "<<arr[i]<<endl;
+    }
+}
+
+void angka::isiData(){
+    for(int i=1;i<=panjang;i++){
+        cout<<i<<" = ";
+        cin>>arr[i];
+    }
+    cout<<endl;
+}
+
+int main(){
+    angka belajarcpp(3);
+    angka *ptrBelajarcpp = new angka(5);
+    delete ptrBelajarcpp;
+
+    return 0;
+}
